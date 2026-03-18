ab0cf75a3e3411f0fdd14762613e3dfa7cb20fd9
ssathishk33 <ssathishk33@gmail.com>
2026-03-18 08:17:25 +0530

    Update FlashUSDT contract to use new imports and mint function

--- FlashUSDT.sol
+++ FlashUSDT.sol
@@ -1,11 +1,20 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20; // Updated version
 
-pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract FlashUSDT is ERC20, Ownable {
+    // Note: OZ 5.0+ requires passing an address to Ownable
+    constructor() ERC20("Tether USD", "USDT") Ownable(msg.sender) {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
+    }
 
-contract FlashUSDT is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Flash USDT", "FUSDT") {
-        _mint(msg.sender, initialSupply);
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6; 
     }
 }
